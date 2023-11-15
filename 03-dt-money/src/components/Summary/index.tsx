0fdda872aa8edd052084dade986f058fcdb2f633
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-11-15 11:46:35 -0300

    added hooks, context and validations

--- 03-dt-money/src/components/Summary/index.tsx
+++ 03-dt-money/src/components/Summary/index.tsx
@@ -1,9 +1,14 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { SummaryCard, SummaryContainer } from "./styles";
 import { useTheme } from "styled-components";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+
+import { useSummary } from "../../hooks/useSummary";
+import { priceFormatter } from "../../utils/formatter";
+
+import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
   const theme = useTheme();
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
@@ -13,7 +18,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={theme["green-300"]} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -22,7 +27,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={theme["red-300"]} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -31,7 +36,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={theme["white"]} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
